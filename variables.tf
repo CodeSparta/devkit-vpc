6b22b54bbec9e4113aaff62968b42123cdb4c2a4
jon <jhultz@redhat.com>
2020-07-15 15:07:33 -0700

    abc

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "cidr_blocks" { default = "10.0.0.0/16"}
+variable "cluster_name" { default = "example" }
+variable "aws_region" { default = "us-gov-west-1"}
+variable "default_tags" { default = {} }
+variable "aws_azs" { default = ["a", "b", "c"] }
+variable "vpc_private_subnet_cidrs" { default = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"] }
+variable "vpc_public_subnet_cidrs" { default = ["10.0.7.0/26", "10.0.8.0/26", "10.0.9.0/26"] }
+variable "aws_access_key" { default = "abc"}
+variable "aws_secret_key" { default = "xyz"}
