363598b71dc7e6aa5653adff5fb5dfb2bd1ff16b
usrbinkat <usrbinkat@braincraft.io>
2020-08-04 14:15:18 -0600

    update branding

--- devkit-destroy-vpc.sh
+++ devkit-destroy-vpc.sh
@@ -2,7 +2,7 @@
 clear
 sudo podman run -it --rm \
     --name devkit-vpc \
-    --entrypoint ./destroy.yml \
+    --entrypoint ./breakdown.yml \
     --workdir /root/deploy/terraform/devkit-vpc \
     --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
   docker.io/codesparta/konductor
