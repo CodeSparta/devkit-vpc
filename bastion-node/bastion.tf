ac455bee44762ccb8a63b2a5f099f98a5a543764
jon <jhultz@redhat.com>
2020-08-10 09:32:00 -0700

    refactoring dev-kit Fixed sg for registry-node

--- bastion-node/bastion.tf
+++ bastion-node/bastion.tf
@@ -1,15 +1,3 @@
-data "aws_vpc" "cluster_vpc" {
-  id =  var.vpc_id
-}
-
-data "aws_subnet" "master-subnets" {
-  filter {
-    name   = "tag:Name"
-    values = [format("${var.cluster_name}-public-%s", format("%s%s", var.aws_region, element(var.aws_azs, 0)))]
-  }
-}
-
-
 resource "aws_instance" "bastion-node" {
   ami           = var.bastion_ami
   instance_type = var.bastion_type
