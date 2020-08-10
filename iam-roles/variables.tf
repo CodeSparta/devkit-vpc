e0bc8d31e4df468cc191919611dcc8e1dd9f2c46
jon <jhultz@redhat.com>
2020-08-10 09:07:52 -0700

    refactoring dev-kit Fixed sg for registry-node

--- iam-roles/variables.tf
+++ iam-roles/variables.tf
@@ -7,5 +7,4 @@ variable "vpc_private_subnet_cidrs" { default = [""] }
 variable "vpc_public_subnet_cidrs" { default = [""] }
 variable "aws_access_key" { default = ""}
 variable "aws_secret_key" { default = ""}
-variable "vpc_id" {}
 variable "vpc_cidr" {default = "" }
