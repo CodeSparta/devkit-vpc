27115696d500db47fb6b4613bb164a4f5d0549bb
jon <jhultz@redhat.com>
2020-08-11 12:49:45 -0700

    refactor data.tf and add ssh key name to main.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,10 @@ variable "aws_region" { default = "us-gov-west-1" }
 
 // Declare Preconfigured AWS Account SSH Private Key Name
 // https://docs.aws.amazon.com/ground-station/latest/ug/create-ec2-ssh-key-pair.html
-variable "aws_ssh_key" { default = "aws_acct_key_name"}
+variable "aws_ssh_key" {
+  type = string
+  default = "aws_acct_key_name"
+}
 
 // COPY & PASTE full id-rsa string.
 // For example cat ~/.ssh/id_rsa.pub or your key of choice
