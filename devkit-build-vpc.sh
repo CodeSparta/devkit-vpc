11028c0383cec66c8af7ad93206486f1231985d0
usrbinkat <usrbinkat@braincraft.io>
2020-08-12 20:29:13 +0000

    minimal hitl wip

--- devkit-build-vpc.sh
+++ devkit-build-vpc.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 clear
+mkdir aws
 sudo rm -rf terraform.tfstate* .terraform
 sudo podman run -it --rm \
     --name devkit-vpc \
     --entrypoint ./site.yml \
     --workdir /root/deploy/terraform/devkit-vpc \
+    --volume $(pwd)/aws:/root/.aws:z \
     --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
-  docker.io/codesparta/konductor
+  docker.io/codesparta/konductor $@
