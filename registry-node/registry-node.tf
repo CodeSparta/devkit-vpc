c4e052bbfc7a4dec81aeca0731fcd35cac1ca897
quantumcatz <30711361+hultzj@users.noreply.github.com>
2020-08-25 11:50:01 -0700

    Update registry-node.tf

--- registry-node/registry-node.tf
+++ registry-node/registry-node.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "registry-node" {
   ami = var.rhcos_ami
   instance_type = var.registry_type
   subnet_id = local.instance_subnet_id
-  user_data = "{\"ignition\":{\"config\":{},\"security\":{\"tls\":{}},\"timeouts\":{},\"version\":\"2.2.0\"},\"networkd\":{},\"passwd\":{\"users\":[{\"name\":\"core\",\"sshAuthorizedKeys\":[\"${var.ssh_public_key}}\"]}]},\"storage\":{},\"systemd\":{}}"
+  user_data = "{\"ignition\":{\"config\":{},\"security\":{\"tls\":{}},\"timeouts\":{},\"version\":\"2.2.0\"},\"networkd\":{},\"passwd\":{\"users\":[{\"name\":\"core\",\"sshAuthorizedKeys\":[\"${var.ssh_public_key}\"]}]},\"storage\":{},\"systemd\":{}}"
 
   root_block_device { volume_size = var.registry_volume }
   security_groups = var.registry_sg_ids
