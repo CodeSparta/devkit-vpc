7272d633cb4149ae1c5e099e58a8f92cedeb2f06
usrbinkat <usrbinkat@braincraft.io>
2020-11-17 10:43:23 -0800

    update image source

--- tools/dev.sh
+++ tools/dev.sh
@@ -8,4 +8,4 @@ sudo podman run -it --rm \
     --workdir /root/deploy/terraform/devkit-vpc \
     --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
     --volume $(pwd)/aws/:/root/.aws/:z \
-  docker.io/codesparta/konductor
+  docker.io/cloudctl/konductor
