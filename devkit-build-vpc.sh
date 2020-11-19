02ef066029c0251b5564d06eab67d6bdda7310c1
Kathryn Morgan <kat@braincraft.io>
2020-11-19 11:13:23 -0800

    cleanup

--- devkit-build-vpc.sh
+++ devkit-build-vpc.sh
@@ -2,10 +2,10 @@
 clear
 mkdir aws 2>/dev/null
 #sudo rm -rf terraform.tfstate* .terraform
-sudo podman run -it --rm \
+podman run -it --rm \
     --name devkit-vpc \
     --entrypoint ./site.yml \
     --workdir /root/deploy/terraform/devkit-vpc \
     --volume $(pwd)/aws:/root/.aws:z \
     --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
-  docker.io/codesparta/konductor $@
+  docker.io/cloudctl/konductor $@
