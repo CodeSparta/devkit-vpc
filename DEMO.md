678be25c2cf9e9d0b8f22ecd410f70af41412ca9
Jon Hultz <jhultz@redhat.com>
2021-08-24 09:06:55 -0700

    pulumi reset

--- DEMO.md
+++
@@ -1,1 +0,0 @@
-[![asciicast](https://asciinema.org/a/t5oc0FfBZSsNdZAKlrcVQQ1IC.png)](https://asciinema.org/a/t5oc0FfBZSsNdZAKlrcVQQ1IC)
--- devkit-destroy-vpc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-sudo podman run -it --rm \
-    --name devkit-vpc \
-    --entrypoint ./breakdown.yml \
-    --workdir /root/deploy/terraform/devkit-vpc \
-    --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
-    --volume $(pwd)/aws:/root/.aws:z \
-  quay.io/cloudctl/konductor:4.7.9
