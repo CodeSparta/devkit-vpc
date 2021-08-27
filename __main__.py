985ffc8c4d7eb6bb12ae725c2b690b346cbcbde3
Jon Hultz <jhultz@redhat.com>
2021-08-27 13:02:08 -0700

    endpoint and SG

--- __main__.py
+++ __main__.py
@@ -230,7 +230,11 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
     vpc_id=shared_vpc.id,
     service_name="com.amazonaws.us-gov-west-1.ec2",
     vpc_endpoint_type="Interface",
-    subnet_ids=[private_subnet.id[0],private_subnet.id[1],private_subnet.id[2]],
+    subnet_ids=[{
+      private_subnet.id[0],
+      private_subnet.id[1],
+      private_subnet.id[2]
+      }],
     security_group_ids=[endpoint_sg.id],
     tags={
         "Name": config.require('cluster_name') + "-ec2-endpoint",
@@ -243,8 +247,12 @@ elb_vpc_endpoint = aws.ec2.VpcEndpoint("elb",
     vpc_id=shared_vpc.id,
     service_name="com.amazonaws.us-gov-west-1.elasticloadbalancing",
     vpc_endpoint_type="Interface",
-    subnet_ids=[private_subnet.id[0],private_subnet.id[1],private_subnet.id[2]],
-    security_group_ids=[endpoint_sg.id],
+    subnet_ids=[{
+      private_subnet.id[0],
+      private_subnet.id[1],
+      private_subnet.id[2]
+      }],
+      security_group_ids=[endpoint_sg.id],
     tags={
         "Name": config.require('cluster_name') + "-elb-endpoint",
         "kubernetes.io/cluster/" + config.require('cluster_name'): "owned"
