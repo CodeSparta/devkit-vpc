8aa4d6ba6c758bc3f16557318549656bfe150cf4
Jon Hultz <jhultz@redhat.com>
2021-08-27 13:32:23 -0700

    endpoint and SG

--- __main__.py
+++ __main__.py
@@ -230,8 +230,7 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
     vpc_id=shared_vpc.id,
     service_name="com.amazonaws.us-gov-west-1.ec2",
     vpc_endpoint_type="Interface",
-    subnet_ids=[
-      private_subnet.id[0]],
+    subnet_ids=[private_subnet.id[0]],
     security_group_ids=[endpoint_sg.id],
     tags={
         "Name": config.require('cluster_name') + "-ec2-endpoint",
@@ -241,14 +240,12 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
 
 sn_ec2_1 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_1",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
-    subnet_id=[private_subnet.id[1]
-      ]
+    subnet_id=[private_subnet.id[1]]
     )
 
 sn_ec2_2 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_2",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
-    subnet_id=[private_subnet.id[2]
-      ]
+    subnet_id=[private_subnet.id[2]]
     )
 
 # ELB endpoint
@@ -267,14 +264,12 @@ elb_vpc_endpoint = aws.ec2.VpcEndpoint("elb",
 
 sn_elb_1 = aws.ec2.VpcEndpointSubnetAssociation("snElb_1",
     vpc_endpoint_id=elb_vpc_endpoint.id,
-    subnet_id=[private_subnet.id[1]
-      ]
+    subnet_id=[private_subnet.id[1]]
     )
 
 sn_elb_2 = aws.ec2.VpcEndpointSubnetAssociation("snElb_2",
     vpc_endpoint_id=elb_vpc_endpoint.id,
-    subnet_id=[private_subnet.id[2]
-      ]
+    subnet_id=[private_subnet.id[2]]
     )
 
 pulumi.export("pulumi-az-amount", zones_amount)
