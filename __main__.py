0a7e449a51d3b3eede0b31ea01d4340d5cd9d3b2
Jon Hultz <jhultz@redhat.com>
2021-08-27 13:07:07 -0700

    endpoint and SG

--- __main__.py
+++ __main__.py
@@ -242,6 +242,15 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
       }
     )
 
+sn_ec2 = aws.ec2.VpcEndpointSubnetAssociation("snEc2",
+    vpc_endpoint_id=ec2_vpc_endpoint.id,
+    subnet_id=[
+      private_subnet.id[0],
+      private_subnet.id[1],
+      private_subnet.id[2]
+      ]
+    )
+
 # ELB endpoint
 elb_vpc_endpoint = aws.ec2.VpcEndpoint("elb",
     vpc_id=shared_vpc.id,
