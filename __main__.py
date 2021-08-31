47e0dab3ad8a4a70bf0a7035182169cedd6743b9
Jon Hultz <jhultz@redhat.com>
2021-08-31 16:59:58 -0700

    add route53 registry record

--- __main__.py
+++ __main__.py
@@ -247,14 +247,14 @@ s3_vpc_endpoint = aws.ec2.VpcEndpoint("s3",
         "kubernetes.io/cluster/" + config.require('cluster_name'): "owned"
       }
     )
-"""
+
 # Need to gather more information, currently cannot get more than 1 subnet per interface assigned...Pulumi slack, waiting for responses
 # EC2 endpoint
 ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
     vpc_id=shared_vpc.id,
     service_name="com.amazonaws.us-gov-west-1.ec2",
     vpc_endpoint_type="Interface",
-    subnet_ids=[private_subnet.id],
+    subnet_ids=[private_subnet.id[0]],
     security_group_ids=[endpoint_sg.id],
     tags={
         "Name": config.require('cluster_name') + "-ec2-endpoint",
@@ -264,9 +264,10 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
 
 sn_ec2_1 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_1",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
-    subnet_id=private_subnet.id
+    subnet_id=[private_subnet.id[1]]
     )
 
+"""
 sn_ec2_2 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_2",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
     subnet_id=private_subnet.id
