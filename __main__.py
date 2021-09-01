f14afc55714833e90947619073513e5f1089c55b
Jon Hultz <jhultz@redhat.com>
2021-09-01 14:40:57 -0700

    update vpc endpoint configuration

--- __main__.py
+++ __main__.py
@@ -265,15 +265,15 @@ ec2_vpc_endpoint = aws.ec2.VpcEndpoint("ec2",
 
 sn_ec2_1 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_1",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
-    subnet_id=private_subnet.id
+    subnet_id=private_subnet_ids[0]
     )
 
-
+"""
 sn_ec2_2 = aws.ec2.VpcEndpointSubnetAssociation("snEc2_2",
     vpc_endpoint_id=ec2_vpc_endpoint.id,
     subnet_id=private_subnet.id
     )
-"""
+
 
 # ELB endpoint
 elb_vpc_endpoint = aws.ec2.VpcEndpoint("elb",
