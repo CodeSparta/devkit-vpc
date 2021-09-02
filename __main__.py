ac791c3882ee4f0db458b800db82bb5c402d65da
Jon Hultz <jhultz@redhat.com>
2021-09-01 17:18:21 -0700

    update vpc endpoint configuration

--- __main__.py
+++ __main__.py
@@ -275,7 +275,6 @@ elb_vpc_endpoint = aws.ec2.VpcEndpoint("elb",
     vpc_id=shared_vpc.id,
     service_name="com.amazonaws.us-gov-west-1.elasticloadbalancing",
     vpc_endpoint_type="Interface",
-    subnet_ids=[private_subnet.id[0]],
     security_group_ids=[endpoint_sg.id],
     tags={
         "Name": config.require('cluster_name') + "-elb-endpoint",
