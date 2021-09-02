b294fc06d276182bea1ffc575c8574191105cbd7
Jon Hultz <jhultz@redhat.com>
2021-09-02 14:37:41 -0700

    add userdata

--- __main__.py
+++ __main__.py
@@ -418,7 +418,7 @@ registry_host=aws.ec2.Instance("registry",
         volume_size=120,
         volume_type="gp3"
     ),
-    user_data=a,
+    user_data=:"a",
   tags={
     "Name": config.require('cluster_name') + "-registry-node",
     "kubernetes.io/cluster/" + config.require('cluster_name'): "owned"
